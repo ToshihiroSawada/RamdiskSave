a8bb9fbe23b2676a25af3322abb4d33c90b451ae
sawada <use.github.mail@gmail.com>
2026-03-16 00:13:29 +0900

    Ver 0.0.1

--- ramdisk-save/src/main.py
+++ ramdisk-save/src/main.py
@@ -1,2 +1,24 @@
-import pathlib
+"""main prosess"""
 
+import pathlib
+import subprocess
+import sys
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+import settings
+
+
+def list_dirs(path):
+    list_dirs = []
+    for d in pathlib.Path(path).iterdir():
+        print(d)
+        if d.is_dir():
+            list_dirs.append(d.name)
+    return list_dirs
+
+
+src_dirs = list_dirs(settings.src)
+dst_dirs = list_dirs(settings.dst)
+
+if src_dirs == dst_dirs:
+    subprocess.call(settings.cmd, shell=True)
